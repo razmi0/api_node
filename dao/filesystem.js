12472c5cd6d8905949b9d14a42d9bf6a3007e7ea
razmiO <thomas.cuesta31@gmail.com>
2024-12-09 11:35:18 +0100

    update filesystem with reservation handleing

--- dao/filesystem.js
+++ dao/filesystem.js
@@ -4,20 +4,42 @@ import { fileURLToPath } from "url";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
-const dataPath = path.join(dirname, "../data/parking.json");
+const parkingPath = path.join(dirname, "../data/parking.json");
+const reservationPath = path.join(dirname, "../data/reservation.json");
 
-export const readJson = async () => {
+// PARKING
+// --
+export const readParkingJson = async () => {
   try {
-    const data = await fs.readFile(dataPath, "utf8");
+    const data = await fs.readFile(parkingPath, "utf8");
     return JSON.parse(data);
   } catch (error) {
     throw new Error("Error reading JSON file");
   }
 };
 
-export const writeJson = async (data) => {
+export const writeParkingJson = async (data) => {
   try {
-    await fs.writeFile(dataPath, JSON.stringify(data, null, 2));
+    await fs.writeFile(parkingPath, JSON.stringify(data, null, 2));
+  } catch (error) {
+    throw new Error("Error writing JSON file");
+  }
+};
+
+// RESERVATION
+// --
+export const readReservationJson = async () => {
+  try {
+    const data = await fs.readFile(reservationPath, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    throw new Error("Error reading JSON file");
+  }
+};
+
+export const writeReservationJson = async (data) => {
+  try {
+    await fs.writeFile(reservationPath, JSON.stringify(data, null, 2));
   } catch (error) {
     throw new Error("Error writing JSON file");
   }
