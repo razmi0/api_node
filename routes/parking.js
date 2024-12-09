a8544e05c22dadc927e6e2d7b20686e8eaa7c460
razmiO <thomas.cuesta31@gmail.com>
2024-12-09 13:25:27 +0100

    add delete reservation in parking route and get details about a reservations for a parking

--- routes/parking.js
+++ routes/parking.js
@@ -1,5 +1,6 @@
 import express from "express";
-import service from "../service/parking.js";
+import serviceParking from "../service/parking.js";
+import serviceReservation from "../service/reservation.js";
 
 const router = express.Router();
 
@@ -12,7 +13,7 @@ const codes = {
 
 router.get("/", async (req, res) => {
   try {
-    const parkings = await service.getAllParkings();
+    const parkings = await serviceParking.getAllParkings();
     res.json(parkings);
   } catch (error) {
     console.log(error);
@@ -22,7 +23,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const parking = await service.getParkingById(req.params.id);
+    const parking = await serviceParking.getParkingById(req.params.id);
     if (!parking) {
       return res.status(codes.notFound).json({ error: "Parking not found" });
     }
@@ -32,9 +33,27 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.get("/:id/reservations", async (req, res) => {
+  try {
+    const parking = await serviceParking.getParkingById(req.params.id);
+    if (!parking) {
+      return res.status(codes.notFound).json({ error: "Parking not found" });
+    }
+    const reservationsIds = parking.reservation;
+    console.log(reservationsIds);
+    const reservations = await Promise.allSettled(
+      reservationsIds.map((id) => serviceReservation.getReservationById(id))
+    );
+    const json = reservations.map((r) => (r.status === "rejected" ? { error: r.reason.message } : r.value));
+    res.json(json);
+  } catch (error) {
+    res.status(codes.internal).json({ error: error.message });
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
-    const newParking = await service.createParking(req.body);
+    const newParking = await serviceParking.createParking(req.body);
     res.status(codes.created).json(newParking);
   } catch (error) {
     res.status(codes.internal).json({ error: error.message });
@@ -43,7 +62,7 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   try {
-    const updatedParking = await service.updateParking(req.params.id, req.body);
+    const updatedParking = await serviceParking.updateParking(req.params.id, req.body);
     if (!updatedParking) {
       return res.status(codes.notFound).json({ error: "Parking not found" });
     }
@@ -55,11 +74,32 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
-    const success = await service.deleteParking(req.params.id);
+    const success = await serviceParking.deleteParking(req.params.id);
     if (!success) {
       return res.status(codes.notFound).json({ error: "Parking not found" });
     }
     res.status(codes.ok).json({ success: true });
+  } catch (error) {
+    res.status(codes.internal).json({ error: error.message });
+  }
+});
+
+router.delete("/:parking_id/reservations/:reservation_id", async (req, res) => {
+  try {
+    const parkingId = req.params.parking_id;
+    const reservationId = req.params.reservation_id;
+    const parking = await serviceParking.getParkingById(parkingId);
+    const reservation = parking.reservation.find((id) => id == reservationId);
+    if (!reservation) {
+      return res.status(codes.notFound).json({ message: `No reservation ${reservationId} in parking ${parkingId}` });
+    }
+    const updatedParking = await serviceParking.updateParking(parkingId, {
+      reservation: parking.reservation.filter((id) => id != reservationId),
+    });
+    if (updatedParking === null) {
+      res.status(codes.internal).json({ success: false, message: "Internal error" });
+    }
+    res.json({ success: true, data: updatedParking });
   } catch (error) {
     res.status(codes.internal).json({ error: error.message });
   }
